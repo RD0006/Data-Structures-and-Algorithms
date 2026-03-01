2f2735053e185c9dad33d0f842ebfdfea1205d38
RD0006 <radhikadn2022@gmail.com>
2026-03-01 17:12:31 +0530

    Update StringQuestions.java

--- String/StringQuestions.java
+++ String/StringQuestions.java
@@ -4,6 +4,11 @@ Maximum Substrings with Distinct Start
 Final Value of Variable After Performing Operations
 Find Words Containing Character
 Minimum Number of Operations to Move All Balls to Each Box
+Defanging an IP Address
+Jewels and Stones
+Reverse String Prefix
+Find Most Frequent Vowel and Consonant
+Convert Date to Binary
 """
 
 import java.util.HashSet;
@@ -78,5 +83,77 @@ public class StringQuestions {
         }
 
         return result;
+    }
+
+    public String defangIPAddr(String address) {
+        StringBuffer sb = new StringBuffer();
+
+        for (char i : address.toCharArray()) {
+            if (i == '.') 
+                sb.append("[.]");
+            else
+                sb.append(i);
+        }
+
+        return sb.toString();
+    }
+
+    public int numJewelsInStones(String jewels, String stones) {
+
+        int cnt = 0; 
+
+        for (stone: stones.toCharArray()) 
+            if (jewels.indexOf(stone) != -1) 
+                cnt++;
+
+        return cnt;
+    }
+
+    public String reversePrefix(String s, int k) {
+        char [] arr = s.toCharArray();
+
+        for (int i = 0; i < k / 2; i++) {
+            char temp = arr[i];
+            arr[i] = arr[k - i - 1];
+            arr[k - i - 1] = temp;
+        }
+
+        return new String(arr);
+    }
+
+    public int maxFreqSum(String s) {
+        int [] arr = new int[26];
+
+        int a = 'a';
+
+        for (int i = 0; i < s.length(); i++)
+            arr[s.charAt(i) - a]++;
+
+        int max_v = 0;
+        int max_c = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (i == 'a' - a || i == 'e' - a || i == 'i' - a || i == 'o' - a || i == 'u' - i) {
+                if (max_v > arr[i]) max_v = arr[i];
+            }
+            else {
+                if (max_c > arr[i]) max_c = arr[i];
+            }
+        }
+
+        return max_v + max_c;
+    }
+
+    public String convertDateToBinary(String date) {
+        String [] date_split = date.split("-");
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < 3; i++) {
+            sb.append(Integer.toBinaryString(Integer.parseInt(date_split[i])));
+            if (i != 2) sb.append("-");
+        }
+
+        return sb.toString();
     }
 }
