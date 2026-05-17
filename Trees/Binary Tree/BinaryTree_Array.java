1a7c9a225d7b672e1ecdd6b4f5eacb3577f76574
RD0006 <radhikadn2022@gmail.com>
2026-05-17 19:25:46 +0530

    Intial Commit - Bianry Trees

---
+++ Trees/Binary Tree/BinaryTree_Array.java
@@ -0,0 +1,118 @@
+class BinaryTree_Array {
+    
+    BinaryTree_Array() {
+
+    }
+
+    void isEmpty() {
+
+    }
+
+    void height() {
+
+    }
+
+    void getNumberOfLeafNodes() {
+
+    }
+
+    void getNumberOfInternalNodes() {
+
+    }
+
+    void insert() {
+
+    }
+
+    void delete() {
+
+    }
+
+    void getNumberOfNodes() {
+
+    }
+
+    void inorder() {
+
+    }
+
+    void preorder() {
+
+    }
+
+    void postorder() {
+
+    }
+
+    void bfs() {
+
+    }
+
+    void dfs() {
+
+    }
+
+    void containsNode() {
+        
+    }
+
+    void searchNodeBFS() {
+
+    }
+
+    void searchNodeDFS() {
+
+    }
+
+    void findParentBFS() {
+
+    }
+
+    void findParentDFS() {
+
+    }
+
+    void getLevelOfNode() {
+
+    }
+
+    void getNumberOfNodesAtLevel() {
+
+    }
+
+    void getDiameter() {
+
+    }
+
+    void topView() {
+
+    }
+
+    void bottomView() {
+
+    }
+
+    void leftView() {
+
+    }
+
+    void rightView() {
+
+    }
+
+    void isBalanced() {
+
+    }
+
+    void isComplete() {
+
+    }
+
+    void isFull() {
+
+    }
+
+    void isPerfect() {
+
+    }
+}
